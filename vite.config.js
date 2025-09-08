260acb6f06f47372dae73fb9968400f9504a8bc6
연수 정 <yunsu0224@gmail.com>
2025-09-08 09:11:47 +0900

    class-portfolio

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/class-portfolio/", // 여기에 Repository 경로를 추가
+
+})
